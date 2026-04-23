c7195003f4e9ef5da885ae3049225cf76ebfca8d
alishanagati10-creator <alishanagati10@gmail.com>
2026-04-23 11:17:03 +0530

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,36 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+
+// Update UI
+function updateDisplay() {
+  countDisplay.textContent = count;
+
+  // Conditional styling
+  if (count > 0) {
+    countDisplay.style.color = "green";
+  } else if (count < 0) {
+    countDisplay.style.color = "red";
+  } else {
+    countDisplay.style.color = "black";
+  }
+}
+
+// Event Listeners
+increaseBtn.addEventListener("click", () => {
+  count++;
+  updateDisplay();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  count--;
+  updateDisplay();
+});
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  updateDisplay();
+});
